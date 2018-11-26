931cfe6e8ba7a4b826b73fdba79348c5b8b19745
zsv <zsv@linuxwizard.ru>
2018-11-26 15:36:17 +0300

    first commit

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-
+import Button from '@material-ui/core/Button';
 class Layout extends Component {
     render() {
         return (
           <div className="wrapper">
             <header className="header">Header</header>
             <main className="main" role="main">{this.props.children}</main>
-            <footer className="footer">Footer</footer>
+            <footer className="footer">
+            Footer
+            <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+            </footer>
           </div>
         )
     }
